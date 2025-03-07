c9aa76ad9df93349c66d845fdebf47707f7c42a7
ochoatadeoaib.cbtis13@gmail.com <ochoatadeoaib.cbtis13@gmail.com>
2025-03-07 10:39:51 -0600

    rutas en flask01

--- flask01/app.py
+++ flask01/app.py
@@ -1,8 +1,26 @@
 from flask import Flask
-app = Flask(__name__)
-@app.route('/')
 
+app = Flask(__name__)
+
+@app.route('/')
 def hola_mundo():
     return 'Hola mundo!'
+
+@app.route('/adios')
+def adios_mundo():
+    return 'Adios mundo!'
+
+@app.route('/hola')
+def hola_html():
+    return '<h1>Hola tu!</h1>'
+
+@app.route('/json')
+def algo():
+    return '{"nombre":"john"}'
+
+@app.route('/xml')
+def xml():
+    return '<?xml version="1.0"?><nombre>john</nombre>'
+
 if __name__== '__main__':
     app.run(host='0.0.0.0', debug=True)
